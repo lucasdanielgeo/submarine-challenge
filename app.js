d2865ab30d58b33ef825cffc0c3cbe863174f7ba
lucasdanielgeo <lucasdaniel.geo@gmail.com>
2020-09-26 21:41:59 -0300

    update styles

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ function submarineFactory(x=0, y=0, z=0, direction ='NORTE') {
         i = i % this.direction_array.length;
         return this.direction_array[i];
     };
-
-    submarine.nextDirection = nextDirection;
+    submarine.nextDirection = nextDirection; // Defindo a função como método do objeto
 
     function prevDirection(){
         i = this.direction_array.indexOf(this.direction);
@@ -23,19 +22,65 @@ function submarineFactory(x=0, y=0, z=0, direction ='NORTE') {
         i = i -1;
         return this.direction_array[i];
     };
-
     submarine.prevDirection = prevDirection;
+    
+    function moveSubmarine(command){
+        command = Array.from(command)
+        return command
+    };
+    submarine.moveSubmarine = moveSubmarine;
 
+    function turnLeft(command) {
+        if (command.toUpperCase() === 'L') {
+            console.log(`direção do submarino agora é ${this.prevDirection().toLowerCase()}.`);
+            this.direction = this.prevDirection();
+            return this.direction;
+        }
+    }
+    submarine.turnLeft = turnLeft;
+
+    function turnRight(command) {
+        if (command.toUpperCase() === 'R') {
+            console.log(`direção do submarino agora é ${this.nextDirection().toLowerCase()}.`);
+            this.direction = this.nextDirection();
+            return this.direction;
+        }
+    }
+    submarine.turnRight = turnRight;
+
+    function moveDown(command) {
+        if (command.toUpperCase()=== 'D') {
+            this.z = this.z - 1
+            console.log(`O eixo z é igual a ${this.z}`)
+            return this.z;
+        }
+    }
+    submarine.moveDown = moveDown;
+
+    function moveUp(command) {
+        if (this.z === 0) {
+            console.log(`Você não pode ultrapassar o limite do oceano, reveja os comandos 'U'`)
+        } else if (command.toUpperCase() === 'U') {
+            this.z =+ 1;
+            console.log(`O eixo z é igual a ${this.z}`)
+            return this.z
+        }
+    }
+    submarine.moveUp = moveUp
+
+    function moveSubmarine(command) {
+        commandArray = Array.from(command)
+        for (let characters = 0; characters < command.length; characters++) {
+            const element = command[characters];
+            
+        }
+    }
+    
     return submarine;
 }
 
 sub = submarineFactory()
-
-
-command = InputEvent
-
-function moveSubmarine(command){
-    command = Array.from(command)
-    return command
-};
-export submarineFactory
+sub.turnRight('r')
+sub.moveDown('d')
+sub.moveUp('u')
+sub.turnLeft('l')
